deffbf61d9606c5c90a54a64dd7d0cc522116723
even311379 <even311379@hotmail.com>
2023-04-19 13:13:29 +0800

    more on deploy

--- FishwaySite/settings/dev.py
+++ FishwaySite/settings/dev.py
@@ -2,13 +2,13 @@ from .base import *
 from SECRET_KEYS import DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
